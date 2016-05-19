d1c8b49aee722e9f09b7754e06434eae98750d71
Manoj Pandey <manojpandey1996@gmail.com>
2016-05-20 03:37:08 +0530

    isotope-magnific fix

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('.popup-gallery').magnificPopup({
-        delegate: 'a',
+        // mighty fix to show only filtered items
+        delegate: ':not(.isotope-hidden).item a',
         type: 'image',
         tLoading: 'Loading image #%curr%...',
         mainClass: 'mfp-img-mobile',
@@ -11,9 +12,9 @@ $(document).ready(function() {
         },
         image: {
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-            titleSrc: function(item) {
-                return item.el.attr('title') + '<small>by Martin Grasser</small>';
-            }
+            // titleSrc: function(item) {
+            //     return item.el.attr('title') + '<small>by Martin Grasser</small>';
+            // }
         }
     });
 });
