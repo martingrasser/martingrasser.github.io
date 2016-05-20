9fbc9a54737030ac4a157570bef3be92e1fe0d5b
Manoj Pandey <manojpandey1996@gmail.com>
2016-05-20 05:35:29 +0530

    button active state

--- js/popup.js
+++ js/popup.js
@@ -12,9 +12,10 @@ $(document).ready(function() {
         },
         image: {
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-            // titleSrc: function(item) {
-            //     return item.el.attr('title') + '<small>by Martin Grasser</small>';
-            // }
+            titleSrc: function(item) {
+                // return item.el.attr('title');
+                return item.el.attr('title') + '<small>by Martin Grasser</small>';
+            }
         }
     });
 });
