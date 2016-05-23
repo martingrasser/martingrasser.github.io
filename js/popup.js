3d7872544c6e22f3c72464458c8608ef95e5cf6c
Manoj Pandey <manojpandey1996@gmail.com>
2016-05-23 09:04:51 +0530

    button states fixes

--- js/popup.js
+++ js/popup.js
@@ -8,13 +8,14 @@ $(document).ready(function() {
         gallery: {
             enabled: true,
             navigateByImgClick: true,
-            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+            preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+            tCounter: '<span class="mfp-counter"></span>' // markup of counter
         },
         image: {
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
             titleSrc: function(item) {
-                // return item.el.attr('title');
-                return item.el.attr('title') + '<small>by Martin Grasser</small>';
+                // return item.el.attr("title");
+                return item.el.attr("title") + '<small>By Martin Grasser</small>';
             }
         }
     });
